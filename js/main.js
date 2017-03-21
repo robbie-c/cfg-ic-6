4b20f4998bb8914b21d727b81a012ec33ff81b5f
Robbie Coomber <robbie.coomber@gmail.com>
2017-03-21 22:07:52 +0000

    Initial code commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function toggleRed() {
+  $('#second-div').toggleClass('red');
+}
+
+function square(x) {
+  return x * x;
+}
+
+function loadDataAndPopulateTable() {
+  $.getJSON('https://robbie-c.github.io/cfg-ic-6/ajax/data.json', function(data) {
+    var table = $('#my-table');
+    data.rows.forEach(function(number) {
+      table.append("<tr><td>" + number + "</td><td>" + square(number) + "</td></tr>")
+    })
+  })
+}
+
+loadDataAndPopulateTable();
+
+console.log('You opened the console! Try writing "2 + 2" and hitting enter!');
