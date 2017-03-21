0cfa120a067a990f26458ff4d9172450180b058f
Robbie Coomber <robbie.coomber@gmail.com>
2017-03-21 22:15:36 +0000

    Improve table presentation

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function square(x) {
 
 function loadDataAndPopulateTable() {
   $.getJSON('https://robbie-c.github.io/cfg-ic-6/ajax/data.json', function(data) {
-    var table = $('#my-table');
+    var table = $('#my-table-body');
     data.rows.forEach(function(number) {
       table.append("<tr><td>" + number + "</td><td>" + square(number) + "</td></tr>")
     })
